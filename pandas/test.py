eef189530708cd03c613b67bc8c3d94d3039d678
Kenneth Zeng <kennethyzeng@gmail.com>
2024-04-20 08:59:25 -0700

    add pandasfile

---
+++ pandas/test.py
@@ -0,0 +1,11 @@
+#! 
+'''
+Pandas read_sql(), which enables you to read a SQL query or database table into a DataFrame
+need to have Python, Pandas, SQLAlchemy and SQLite and your favorite IDE set up to start coding
+pandas.read_sql(sql, con, index_col=None, coerce_float=True, params=None, parse_dates=None, columns=None, chunksize=None)
+
+
+link: 
+https://www.askpython.com/python-modules/pandas/pandas-read-sql
+
+'''
